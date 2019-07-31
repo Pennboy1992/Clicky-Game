32e5c49691ab2d170a2df99d6df89f13e429dbb7
Mad Marine <pnnroman92@gmail.com>
2019-07-31 00:29:25 -0500

    added HeroCard.js, Header.js, and images.json and got overall layout ready to map images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import heroes from "./images.json"
+import HeroCard from "./HeroCard"
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+        {this.state.heroes.map(heroes => (
+          <HeroCard
+            
+            id={heroes.id}
+            key={heroes.id}
+            
+            image={heroes.image}
+            
+          />
+        ))}
+      
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './App.css';
+
+
+
+function Header() {
+  return(
+    <div>
+      <div className="scoreCard">
+        <h2>Score: 0 | 0 : Highscore</h2>
+      </div>
+      <div className= "jumbotron colorJ">
+        <h1 className= "header">Click Heroes</h1>
+
+      </div>
+
+
+    </div>
+
+
+  )
+
+
+}
+
+export default Header 
