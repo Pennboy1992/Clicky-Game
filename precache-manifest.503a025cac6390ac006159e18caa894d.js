c63e2b435b9f98f4b463b26ba65f926c8728ae7e
Mad Marine <pnnroman92@gmail.com>
2019-08-12 10:30:39 -0500

    Updates

---
+++ precache-manifest.503a025cac6390ac006159e18caa894d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c31466502105fd806b202bb723a801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83f7146b04e881c0f20",
+    "url": "/static/css/main.b3e2183a.chunk.css"
+  },
+  {
+    "revision": "5d25dcab040f0ab74341",
+    "url": "/static/js/2.c2ee0817.chunk.js"
+  },
+  {
+    "revision": "c83f7146b04e881c0f20",
+    "url": "/static/js/main.b5bd9e12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd5ce8fce34c8ba15f7c7ee80233a30d",
+    "url": "/static/media/p4.bd5ce8fc.png"
+  },
+  {
+    "revision": "30b16def0d673e6a38a20aaaeff63262",
+    "url": "/static/media/p8.30b16def.png"
+  }
+]);
